e42378b810d4a6dd9970a554b43cd7b725069d80
virtuo <virtuo@tnpm.sherpa.com>
2015-09-14 17:19:01 +0800

    Add  that always uses pwd

--- reload_lifs.pl
+++ reload_lifs.pl
@@ -6,11 +6,15 @@
 use strict;
 use warnings;
 
-my $HOME_DIR = "/var/tmp/";
+my $CURRENT_DIR = `pwd`;
+chomp($CURRENT_DIR);
+
+my $HOME_DIR = $CURRENT_DIR . "/";
 my $CRIT_LIF = $HOME_DIR . "critical_lif.lif";
 my $CLR_LIF = $HOME_DIR . "cleared_lif.lif";
 
-my $LOADER_DIR = "/appl/virtuo/var/loader/spool/huaweigsmmscs_v200r009c02sph102/V200R009C02SPH102_wei/";
+#my $LOADER_DIR = "/appl/virtuo/var/loader/spool/huaweigsmmscs_v200r009c02sph102/V200R009C02SPH102_wei/";
+my $LOADER_DIR = "/appl/virtuo/var/loader/spool/huaweigsmmscs_v200r009c02sph102/V200R009C02SPH102/";
 my $LOADER_LIF = $LOADER_DIR . "M83888175-#-A-#-10Sep2015-#-13\:15-#-13\:30-#-MSOFTX_H014-#-I-#-RT-#-P-16187454.lif";
 
 my $SLEEP_DUR = 5;
